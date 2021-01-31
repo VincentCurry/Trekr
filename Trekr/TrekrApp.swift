4685362eee17ebaf2fa6b0e2d6b1e8e074bed78c
Vincent Curry <vincentcurry@tutanota.com>
2021-01-31 22:44:56 +0000

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by user186344 on 1/31/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
